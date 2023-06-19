98197172e25b779ccda7257ecfb8104e2a967b0a
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 13:39:02 -0400

    :tada: create project

---
+++ src/main/java/com/example/vehicleregister/VehicleRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicleregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vehicleregister/VehicleRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
