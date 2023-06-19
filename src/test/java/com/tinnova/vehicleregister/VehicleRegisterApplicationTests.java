0f4dc1873bd3a398ac0a851dce9bf9434193fba9
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 17:26:10 -0400

    :white_check_mark: update test

--- src/test/java/com/tinnova/vehicleregister/VehicleRegisterApplicationTests.java
+++ src/test/java/com/tinnova/vehicleregister/VehicleRegisterApplicationTests.java
@@ -1,13 +1,11 @@
 package com.tinnova.vehicleregister;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class VehicleRegisterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
