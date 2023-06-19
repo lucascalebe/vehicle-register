a3ffb026c5baf878bf9385c62b7423e742af8fc9
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 18:42:09 -0400

    :sparkles: create input model

---
+++ src/main/java/com/tinnova/vehicleregister/api/model/input/VehicleInputModel.java
@@ -0,0 +1,29 @@
+package com.tinnova.vehicleregister.api.model.input;
+
+import com.tinnova.vehicleregister.domain.model.VehicleBrand;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Setter
+public class VehicleInputModel {
+
+  @NotBlank
+  private String vehicle;
+
+  @NotNull
+  private VehicleBrand brand;
+
+  @NotNull
+  @Positive
+  private Integer year;
+
+  private String description;
+
+  @NotNull
+  private boolean sold;
+}
