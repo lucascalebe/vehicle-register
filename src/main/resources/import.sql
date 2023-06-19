a0eb241b23b2cbaccccd853dd4140c24e4af523c
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 14:57:57 -0400

    :hammer: add import script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into vehicle (id, vehicle, brand, vehicle_year, description, sold, created_date) values (1, 'ONIX', 'CHEVROLET', '2020', "carro semi novo", false, '2023-06-19 10:30:00-03:00');
+insert into vehicle (id, vehicle, brand, vehicle_year, description, sold, created_date) values (2, 'ARGO', 'FIAT', '2023', "carro novo", false, '2023-06-19 11:00:00-03:00');
