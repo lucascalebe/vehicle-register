8b44eec8d5bac2ff71c3c8940189202866789854
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 17:08:48 -0400

    :sparkles: create assembler

---
+++ src/main/java/com/tinnova/vehicleregister/api/assembler/VehicleResponseModelAssembler.java
@@ -0,0 +1,18 @@
+package com.tinnova.vehicleregister.api.assembler;
+
+import com.tinnova.vehicleregister.api.model.VehicleResponseModel;
+import com.tinnova.vehicleregister.domain.model.Vehicle;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class VehicleResponseModelAssembler {
+
+  private final ModelMapper modelMapper;
+
+  public VehicleResponseModel toModel(Vehicle vehicle) {
+    return modelMapper.map(vehicle, VehicleResponseModel.class);
+  }
+}
