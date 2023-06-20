8fcfc9dd693b8a607aceb0a3945b400e9a203470
Lucas Calebe <lucascalebe07@hotmail.com>
2023-06-19 20:35:57 -0400

    :sparkles: implementing page findAll endpoint

--- src/main/java/com/tinnova/vehicleregister/api/assembler/VehicleResponseModelAssembler.java
+++ src/main/java/com/tinnova/vehicleregister/api/assembler/VehicleResponseModelAssembler.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 @RequiredArgsConstructor
 public class VehicleResponseModelAssembler {
@@ -14,5 +17,9 @@ public class VehicleResponseModelAssembler {
 
   public VehicleResponseModel toModel(Vehicle vehicle) {
     return modelMapper.map(vehicle, VehicleResponseModel.class);
+  }
+
+  public List<VehicleResponseModel> toCollectionModel(List<Vehicle> vehicles) {
+    return vehicles.stream().map(this::toModel).collect(Collectors.toList());
   }
 }
--- src/main/java/com/tinnova/vehicleregister/api/controller/VehicleController.java
+++ src/main/java/com/tinnova/vehicleregister/api/controller/VehicleController.java
@@ -5,8 +5,15 @@ import com.tinnova.vehicleregister.api.assembler.VehicleResponseModelAssembler;
 import com.tinnova.vehicleregister.api.model.VehicleResponseModel;
 import com.tinnova.vehicleregister.api.model.input.VehicleInputModel;
 import com.tinnova.vehicleregister.domain.model.Vehicle;
+import com.tinnova.vehicleregister.domain.repository.VehicleRepository;
+import com.tinnova.vehicleregister.domain.repository.filter.VehicleFilter;
 import com.tinnova.vehicleregister.domain.service.VehicleService;
+import com.tinnova.vehicleregister.infrastructure.repository.spec.VehicleSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +27,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/vehicles", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,8 +35,18 @@ import javax.validation.Valid;
 public class VehicleController {
 
   private final VehicleService vehicleService;
+  private final VehicleRepository vehicleRepository;
   private final VehicleResponseModelAssembler vehicleAssembler;
   private final VehicleInputDisassembler vehicleDisassembler;
+
+  @GetMapping
+  public Page<VehicleResponseModel> findAllVehicles(VehicleFilter filter, @PageableDefault(size = 10) Pageable pageable) {
+    Page<Vehicle> vehiclePage = vehicleRepository.findAll(VehicleSpecs.usingFilter(filter), pageable);
+
+    List<VehicleResponseModel> vehicleResponseModels = vehicleAssembler.toCollectionModel(vehiclePage.getContent());
+
+    return new PageImpl<>(vehicleResponseModels, pageable, vehiclePage.getTotalElements());
+  }
 
   @GetMapping("/{vehicleId}")
   public VehicleResponseModel findVehicle(@PathVariable Long vehicleId) {
